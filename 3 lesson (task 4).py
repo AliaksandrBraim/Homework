b439027153ce5ed1677a66882e98cac8bb66aee2
and <Braim1994@mail.ru>
2023-06-28 18:25:36 +0300

    'homework4'

--- 3 lesson (task 4).py
+++ 3 lesson (task 4).py
@@ -1,8 +1,23 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных
 # и сколько отрицательных
-x, y, z = int(input()), int(input()), int(input())
-b, n, m = abs(x), abs(y), abs(z)
-b = b is x
-n = n is y
-m = m is z
-print(b+n+m)
+# x, y, z = int(input()), int(input()), int(input())
+# b, n, m = abs(x), abs(y), abs(z)
+# b = b is x
+# n = n is y
+# m = m is z
+# print(b+n+m)
+# print(sum([1, 2, 3])/3)
+# number = [(i +2)**3 for i in range(10)]
+# from collections import*
+# user = namedtuple('User', ('name', 'age')
+# a = 5
+# b = 10
+# c = 2
+# numbers = [i for i in range(5, 10, 2)]
+# print(numbers)
+
+
+
+
+
+
---
+++ lesson 4 (task 1).py
@@ -0,0 +1,5 @@
+# Заполнить список степенями числа 2 (от 2^1 до 2^n).
+spisok = [2**i for i in range(1, int(input('n')))]
+
+print(spisok)
+# gfs
